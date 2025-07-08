8e9c56e5301fe31762c3883b9027d7261c1898c1
Esteban Hernán Lobos Canales <esteban.canales@safiraenergia.cl>
2025-07-08 17:46:19 -0400

    avance de cambios en footer y diseño de colores en cada plantilla

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bo&&8hgw!3+o4r%#fou=j%sam1v=*q+hus#8hdtcs&(aqj_ywv'
+SECRET_KEY = 'armh@i!ozgxu1_%&*m4n^00j2jcuaczlgod&peap0!^c_w!3z!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
